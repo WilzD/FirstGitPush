23690a4b6edda948e4621e7ced7a6deb5b3e3ed5
WilzD <wilsondynamic3@gmail.com>
2023-10-17 18:39:40 +0530

    item 2 BG black and font color green

--- dom.js
+++ dom.js
@@ -7,5 +7,7 @@ header.style.borderBottom='solid 5px black'
 let additems=document.getElementsByClassName('list-group-item')
 for(let i=0;i<additems.length;i++){
     additems[i].style.fontWeight='bold'
-    additems[i].style.Color='green'
+    additems[i].style.color='green'
 }
+
+additems[2].style.backgroundColor='black'
